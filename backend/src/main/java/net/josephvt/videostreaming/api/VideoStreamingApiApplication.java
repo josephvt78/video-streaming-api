42ec211a7a55c5b84d40724711e2a622e53bae0b
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-11-07 16:17:43 -0500

    Initial version of the project that was refactored as a multi-module spring boot project

---
+++ backend/src/main/java/net/josephvt/videostreaming/api/VideoStreamingApiApplication.java
@@ -0,0 +1,13 @@
+package net.josephvt.videostreaming.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamingApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/net/josephvt/videostreaming/api/VideoStreamingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.josephvt.videostreaming.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
